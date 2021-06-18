eb7484176a47e6f3669f71f4cab7358212d649e1
linxs <cakelinxs@outlook.com>
2021-06-18 17:44:20 +0800

    feat(design): add pre-step data store

--- base/step.py
+++ base/step.py
@@ -9,25 +9,40 @@ from urllib.parse import urlparse
 from base.suit import Suit
 
 
-class TestStep:
-    def init_resource(self, request_url: str, method: str, data: dict, headers: str, desire_result: dict):
+class Step:
+    def init_resource(self, request_url: str, method: str, data: dict, headers: str, desire_result: dict, pre: dict):
         self.session = SRequest()
         self.method = str(method).upper()
         self.request_url = request_url
         self.headers = headers
         self.data = data
         self.desire_result = desire_result
-        # self.parent = parent
+        self.pre = pre
         self.result = dict()
 
     def assert_result(self, current, desire):
-        pass
+        for k,v in desire:
+            assert current[k] == desire.get(k)
     
     def get_url_path(self) -> str:
         parsed = urlparse(self.request_url)
         return parsed.path
 
+    def setPre(self):
+        if self.pre:
+            if self.pre.get("addTo","empty") == "Headers":
+                headers = self.pre.get("data",dict())
+                for k,_ in headers.items:
+                    self.headers[k] = headers[k]
+                return
+            data = self.pre.get("data",dict())
+            for k,_ in data.items:
+                self.data[k] = data[k]
+            
+        
     def runRequest(self):
+        if self.pre is not None:
+            self.setPre()
         print(f"self.session.{self.method}({self.request_url},{self.data},{self.desire_result['code']},{self.headers})")
         resp, exception = eval(f"self.session.{self.method}('{self.request_url}',{self.data},{self.desire_result['code']}, **{self.headers})")
         print(exception)
