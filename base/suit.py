9464f299f80f786b9e1fd679814924c84ea9a23e
linxs <cakelinxs@outlook.com>
2021-06-18 17:44:56 +0800

    feat(design): add run step

--- base/suit.py
+++ base/suit.py
@@ -1,5 +1,26 @@
+import pytest
+from base.step import Step, TestStep
 
 
 class Suit:
-    def __init__(self):
+    def __init__(self, filepath: str):
         self.step_result = dict()
+        # step:
+        self.steps = self.generate(filepath=filepath)
+        self.result = list()
+
+
+    def generate(self, filepath: str) ->list(Step):
+        return list(Step)
+    
+    def run(self):
+        for step in self.steps:
+            # 初始化step资源
+            step.init_resource()
+            # 执行step
+            step.runRequest()
+            # 验证response
+            step.assert_result()
+            # 验证成功，将step的结果写入到suit.result
+            self.result[step.get_url_path()] = self.result.append(step.result)
+            
