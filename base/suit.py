bf08af217f0187b770bef0e33d7581d53c470097
linxs <cakelinxs@outlook.com>
2021-06-24 17:42:01 +0800

    feat(design): feat Suit

--- base/suit.py
+++ base/suit.py
@@ -1,26 +1,33 @@
+import typing
 import pytest
-from base.step import Step, TestStep
+import sys
+sys.path.append("..")
+
+from step import Step
 
 
 class Suit:
-    def __init__(self, filepath: str):
+    def init_resource(self, steps: typing.List[Step]):
         self.step_result = dict()
-        # step:
-        self.steps = self.generate(filepath=filepath)
-        self.result = list()
+        self.steps = steps
+        self.result = dict()
 
 
-    def generate(self, filepath: str) ->list(Step):
-        return list(Step)
     
     def run(self):
         for step in self.steps:
-            # 初始化step资源
-            step.init_resource()
+            # 动态执行preStep中的$a.b.c()
+            for p in step.pre:
+                p['response'] = eval(f"{p['response'].replace('$suit','self')}")
             # 执行step
-            step.runRequest()
+            step.run()
             # 验证response
             step.assert_result()
             # 验证成功，将step的结果写入到suit.result
-            self.result[step.get_url_path()] = self.result.append(step.result)
-            
+            self.result[step.name] = step.result
+        print(self.result['getTaskLocalConfig'])
+
+
+if __name__ == '__main__':
+    pass
+    
