cda1ccead743a76ad77f75617a8e0367ba8a846f
linxs <cakelinxs@outlook.com>
2021-07-11 01:33:45 +0800

    feat(cli): change cli name

--- cli/setup.py
+++ cli/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sat',
+    name='atctl',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'sat = sat.scripts.demo:cli',
+            'atctl = atctl.scripts.root:atctl',
         ],
     },
 )
