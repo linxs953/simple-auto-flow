43d514f82deb9d6c49c6ba8d23fb40573d611a60
linxs <cakelinxs@outlook.com>
2021-08-30 15:10:52 +0800

    doc: core.utils.step annotation

--- core/simat_core/utils/step.py
+++ core/simat_core/utils/step.py
@@ -1,14 +1,12 @@
-
-
-"""
-通过refer_express去preSteps中寻找要extract的field
-"""
-
-
 import logging
 
 
+"""
+通过refer_express去preSteps中寻找要提取extract的field-expression
+"""
+
 def searchStepInPres(refer_express: str, preSteps: list):
+    # 验证引用的格式，必须包含.字符
     if "." not in refer_express:
         # log: refer format error
         return
@@ -17,12 +15,14 @@ def searchStepInPres(refer_express: str, preSteps: list):
     step = ""
     field = ""
     
-    # 拿到引用的preStep
+    # 获取引用的每个「关键字」
+    # 遍历preStep对象，拿到关键字引用的preStep对象
     for index, st in enumerate(preSteps):
         if st.get("name") == refer_express_parts[0]:
             step = st
             break
         
+        # 列表最后一个元素，找不到关键字引用preStep对象，直接return
         if index+1 == len(preSteps):
             # 这里其实不会出现pre step找不到的情况，在解析yaml的时候会验证引用是否合法有效，这里主要再做个保险
             #  log: not found step
@@ -30,10 +30,16 @@ def searchStepInPres(refer_express: str, preSteps: list):
             return
     
     result = step
+    
+    # 遍历关键字列表，根据每个关键字去step里面的值
+    # result此时拿到的是preStep对象
     for i in range(1,len(refer_express_parts)):
         try:
             if type(result) == list:
                 for index_,f in enumerate(result):
+                    # 处理refer数组
+                    # 如果当前type(result)==list && reuslt == step.refer
+                    #   - 判断express(getToken.refer.token)引用的字段名token在refer中是否可以找到，找到就return
                     if f.get('name') == refer_express_parts[i]:
                         field = f['field'] 
                         return field
@@ -53,16 +59,25 @@ def searchStepInPres(refer_express: str, preSteps: list):
 """
 通过field_refer去接口返回体中提取具体的字段值并返回
 """
+
 def extractField(field_refer: str, body: dict):
+    # 验证引用的格式，必须包含.字符
     if "." not in field_refer:
         #  有可能字段名错误拿不到值，返回None
         field = body.get(field_refer,None)
         return field
+    
+    # refer expression 转换成列表
     field_refer_parts = field_refer.split(".")
     length = len(field_refer_parts)
+    
+    # field：引用表达式所要提取的字段的值
     field = body
+    
+    # 这里处理一下list index和dict key的提取操作
     for key in range(length):
         try:
+            # 当前field_refer_parts[key]是数字，field=field[refer_index]
             refer_index = int(field_refer_parts[key])
             field = field[refer_index]
             continue
@@ -77,6 +92,8 @@ def extractField(field_refer: str, body: dict):
             if field is None:
                 # log: none type object
                 return
+            
+            # 如果field_refer_parts[key]不是数字，而是一个dict key
             if type(field) == dict:
                 field = field.get(field_refer_parts[key], None)
             else:
@@ -109,17 +126,5 @@ if __name__ == '__main__':
 
     field = searchStepInPres("getToken.refer.token",preSteps)
     
-    data = {
-        "body":{
-            "access_token": [
-                {
-                   "value":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiIxMzllNjBiZi00MDgyLTQ4ZTUtYWRiYi1kODZhNTZlMGU2ZGMiLCJ1bmlxdWVfbmFtZSI6IjEzOWU2MGJmLTQwODItNDhlNS1hZGJiLWQ4NmE1NmUwZTZkYyIsImdpdmVuX25hbWUiOiJ4aGEyMjA2NiIsImVtYWlsIjoieGhhMjIwNjZAdHVvZnMuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbW9iaWxlcGhvbmUiOiIiLCJyZWdpc3RlckRhdGUiOiIyMDIwLzgvMzEgMTI6MTc6MTAiLCJsYXN0UGFzc3dvcmRDaGFuZ2VkRGF0ZSI6IjE5NzAvMS8xIDg6MDA6MDAiLCJpc3MiOiJPY3RvcHVzLkF1dGgiLCJhdWQiOiJPY3RvcHVzLkFwaSIsImV4cCI6MTYyNDUwNDU3MiwibmJmIjoxNjI0NDE4MTcyfQ.3Y44E_9snrC2W8ikI3qhcgjgKt-BOXuInRfBglRAxrE"
-                }
-            ],
-        },
-        "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiIxMzllNjBiZi00MDgyLTQ4ZTUtYWRiYi1kODZhNTZlMGU2ZGMiLCJ1bmlxdWVfbmFtZSI6IjEzOWU2MGJmLTQwODItNDhlNS1hZGJiLWQ4NmE1NmUwZTZkYyIsImdpdmVuX25hbWUiOiJ4aGEyMjA2NiIsImVtYWlsIjoieGhhMjIwNjZAdHVvZnMuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbW9iaWxlcGhvbmUiOiIiLCJyZWdpc3RlckRhdGUiOiIyMDIwLzgvMzEgMTI6MTc6MTAiLCJsYXN0UGFzc3dvcmRDaGFuZ2VkRGF0ZSI6IjE5NzAvMS8xIDg6MDA6MDAiLCJpc3MiOiJPY3RvcHVzLkF1dGgiLCJhdWQiOiJPY3RvcHVzLkFwaSIsImV4cCI6MTYyNDUwNDU3MiwibmJmIjoxNjI0NDE4MTcyfQ.3Y44E_9snrC2W8ikI3qhcgjgKt-BOXuInRfBglRAxrE",
-        "token_type": "bearer",
-        "expires_in": 86399,
-        "refresh_token": "9a11d2e7045c497c9cb4a1da27d9f2dc"
-    }
+    data = {}
     data = extractField("body.access_token.0.value",data)
